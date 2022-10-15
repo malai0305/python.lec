447b90a6336944f3925df52c419bf25a6802cba6
unknown <artem03051987@gmail.com>
2022-10-15 22:13:44 +0500

    Управляющие конструкции: while,for

---
+++ lec4.py
@@ -0,0 +1,54 @@
+# Управляющие конструкции: if,if-else
+
+#a=int(input('a='))
+#b=int(input('b='))
+#if a>b:
+ #  print(a)
+#else:    
+ #   print(b)
+    
+#username=input('Введите имя:')
+#if username=='МАША':
+   # print('Ура,это же МАША!')
+#elif username=='Марина':
+    #print('Я так ждала Вас, Марина!')
+#elif username=='Ильнар':
+    #print('Ильнар-топ)') 
+#else:
+   # print('Привет,',username)       
+
+# Управляющие конструкции: while
+#original=23
+#inverted=0
+#while original !=0:
+ #   inverted=inverted*10+(original%10)
+  #  original//=10
+   # print(inverted)    
+#else:
+ #   print('Пожалуй')
+  #  print('хватит)')    
+   # print(inverted)
+
+# Управляющие конструкции: for
+# Когда мы знаем,что хотим 
+
+#for i in 1,2,3,4,5:
+ #   print(i**2)
+
+#list=[1,2,3,4,5]
+#for i in list:
+ #   print(i**2)
+
+#r=range(10)
+#for i in r:
+ #   print(i)
+
+#for i in range(1,10):
+ #   print(i)
+
+#for i in range(1,10,2):
+ #   print(i)
+
+#Строки
+#for i in 'qwe - rty':
+ #   print(i)
