d22d2b7ef1bf8e0cc00fa5beee4cafd2e4436f72
unknown <artem03051987@gmail.com>
2022-10-15 15:06:26 +0500

    типы данных и переменные

---
+++ lec.py
@@ -0,0 +1,14 @@
+#Типы данных и переменные:
+#int,float,boolean,str,list,None.
+
+#s = "q\nwert"
+#print(s)
+#list = [1,2,'3','hello world']
+#print(list)
+print('Введите a');
+a=int(input())
+print('Введите b');
+b=int(input())
+#print(a,'+',b,'=',a+b)
+#print('{} {}'.format(a,b))
+#print(f'{a} {b}')
