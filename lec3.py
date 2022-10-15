8e494bf8b629a9a4a9050facc01abec186d30734
unknown <artem03051987@gmail.com>
2022-10-15 17:19:50 +0500

    Логические операции

---
+++ lec3.py
@@ -0,0 +1,20 @@
+# Логические операции
+# >,>=,<,<=,==,!=
+# not,and,or-не путать с &,|,^
+# Кще-что еще: is,is not,in,not in
+
+#a=1<3 and 2<3
+#print(a)
+
+#a=1<2<3<4>2
+#print(a)
+
+#name=35
+#m=5
+#d=3
+#print(name>m>d)
+
+#f=[1,2,3,4]
+#print(f)
+#is_odd=f[1]%2==0
+#print(is_odd)
